3deac423ef6b9b3f2dc041ea1f0cea1601f73f91
YanMaciel <araujo.yan@hotmail.com>
2022-04-16 19:40:30 -0300

    error page not found handler

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,12 +10,18 @@ def index(request):
     })
 
 def wiki_detail_view(request, entry=None):
-    entry_obj = None
-    if entry is not None:
-        entry_obj = markdown2.markdown(util.get_entry(entry))
-        
-    context = {
-        "title": entry,
-        "object": entry_obj,
-    }
-    return render(request, "encyclopedia/detail.django-html", context=context)
+    try:
+        entry_obj = None
+        if entry is not None:
+            entry_obj = markdown2.markdown(util.get_entry(entry))
+            
+        context = {
+            "title": entry,
+            "object": entry_obj,
+        }
+        return render(request, "encyclopedia/detail.django-html", context=context)
+    except:
+        context = {
+            "title": entry,
+        }
+        return render(request, "encyclopedia/not_found.django-html", context=context)
